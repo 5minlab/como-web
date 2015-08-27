bdb89359ba77f2ba9e85030a6978ed153a85d8f1
if1live <libsora25@gmail.com>
2015-08-27 16:28:57 +0900

    게임 여러개 동시 지원

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ class UnicodeReader:
 
     def __iter__(self):
         return self
-        
-def read_tos_file(filename):
+
+def read_tos_file(filename, game_name, url):
     line_list = []
     with open(filename, 'rb') as csvfile:
         reader = UnicodeReader(csvfile)
@@ -53,23 +53,56 @@ def read_tos_file(filename):
                 continue
             line = unicode(row[3])
             line_list.append(line)
+
+    def replace_game_name(text):
+        src_txt = "Como"
+        dst_txt = game_name
+        return text.replace(src_txt, dst_txt)
+
+    def replace_url(text):
+        src_txt = "http://5minlab.github.io/como-web/tos/"
+        dst_txt = url
+        return text.replace(src_txt, dst_txt)
+
+    line_list = [replace_game_name(replace_url(line)) for line in line_list]
+
     return line_list
 
-    
+@app.route('/')
+def index():
+    return fl.render_template('index.html')
+
 @app.route('/tos/')
-def hello():
-    privacy_file = os.path.join('data', "150213 Como's Adventure 법률 문서 - Privacy Policy.csv")
-    privacy_lines = read_tos_file(privacy_file)
-    
-    tos_file = os.path.join('data', "150213 Como's Adventure 법률 문서 - Terms of Service.csv")
-    tos_lines = read_tos_file(tos_file)
-    
+def tos_como():
     title = "Como"
+    url = "http://5minlab.github.io/como-web/tos/"
+    return render_tos(title, url)
+
+@app.route('/tos/hungry-mates.html')
+def tos_hungry_mates():
+    title = "Hungry Mates"
+    url = "http://5minlab.github.io/como-web/tos/hungry-mates.html"
+    return render_tos(title, url)
+
+@app.route('/tos/slime-slasher.html')
+def tos_slime_slasher():
+    title = 'Slime Slasher'
+    url = "http://5minlab.github.io/como-web/tos/slime-slasher.html"
+    return render_tos(title, url)
+
+def render_tos(title, url):
+    privacy_file = os.path.join('data', "150213 Como's Adventure 법률 문서 - Privacy Policy.csv")
+    privacy_lines = read_tos_file(privacy_file, title, url)
+
+    tos_file = os.path.join('data', "150213 Como's Adventure 법률 문서 - Terms of Service.csv")
+    tos_lines = read_tos_file(tos_file, title, url)
+
     return fl.render_template('tos.html', privacy_lines=privacy_lines, tos_lines=tos_lines, title=title)
-    
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0')
     else:
         if sys.argv[1] == 'freeze':
             freezer.freeze()
